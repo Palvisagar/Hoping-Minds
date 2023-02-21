c8bd14631a088657c73a636891819cc81602889f
palvisagar <pallvisagar.code@gmail.com>
2023-02-21 16:42:25 +0530

    21 feb 2023

--- 17feb2023_Palvi/probability.py
+++ 17feb2023_Palvi/probability.py
@@ -1,30 +1,22 @@
-# program of probability
-# Author Palvi Sagar
-# 17 feb 2023
+# Ask the user to input the number of balls
+num_balls = int(input("Enter the number of balls: "))
 
+# Create an empty dictionary to store the number of balls of each color
+ball_counts = {}
 
+# Loop through each ball and ask the user to input its color
+for i in range(num_balls):
+    ball_color = input(f"Enter the color of ball {i+1}: ")
+    
+    # If the color is not already in the dictionary, add it with a count of 1
+    if ball_color not in ball_counts:
+        ball_counts[ball_color] = 1
+    # If the color is already in the dictionary, increment its count by 1
+    else:
+        ball_counts[ball_color] += 1
 
-# Import the random module
-import random
-
-# Define the total number of balls
-total_balls = 10
-
-# Define the number of red balls
-red_balls = 5
-green_balls = 9
-blue_balls = 6
-
-
-# Calculate the probability of selecting a red ball
-probability = red_balls / total_balls
-p = blue_balls / total_balls
-probabilities = green_balls / total_balls 
-
-# Print the probability
-print("The probability of selecting a red ball is:", probability)
-print("The probability of selecting a blue ball is:", p)
-print("The probability of selecting a green ball is:", probabilities)
-
-
-
+# Calculate the probability of drawing each color of ball
+print("Probability distribution:")
+for color, count in ball_counts.items():
+    probability = count / num_balls
+    print(f"{color}: {probability:.2f}")
--- 20feb2023_Palvi/distribution.py
+++ 20feb2023_Palvi/distribution.py
@@ -1,4 +1,35 @@
-# program of probability
-# Author Palvi Sagar
-# 20 feb 2023
+#Program of distribution of probability
+#Author Palvi Sagar
+import matplotlib.pyplot as plt
 
+# Ask the user to input the number of balls
+num_balls = int(input("Enter the number of balls: "))
+
+# Create an empty dictionary to store the number of balls of each color
+ball_counts = {}
+
+# Loop through each ball and ask the user to input its color
+for i in range(num_balls):
+    ball_color = input(f"Enter the color of ball {i+1}: ")
+    
+    # If the color is not already in the dictionary, add it with a count of 1
+    if ball_color not in ball_counts:
+        ball_counts[ball_color] = 1
+    # If the color is already in the dictionary, increment its count by 1
+    else:
+        ball_counts[ball_color] += 1
+
+# Calculate the probability of drawing each color of ball
+colors = []
+probabilities = []
+for color, count in ball_counts.items():
+    probability = count / num_balls
+    colors.append(color)
+    probabilities.append(probability)
+
+# Create a bar chart of the probability distribution
+plt.bar(colors, probabilities)
+plt.xlabel('Ball Color')
+plt.ylabel('Probability')
+plt.title('Probability Distribution of Ball Colors')
+plt.show()
